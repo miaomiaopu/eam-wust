6171fd1fcd3d50e2939402e14b254d0809ef5e1b
miaomiaopu <1991416497@qq.com>
2023-04-15 19:38:00 +0800

    位于分支 master
    初始提交
    
    要提交的变更：
    	新文件：   .gitignore
    	新文件：   .mvn/wrapper/maven-wrapper.jar
    	新文件：   .mvn/wrapper/maven-wrapper.properties
    	新文件：   README.md
    	新文件：   mvnw
    	新文件：   mvnw.cmd
    	新文件：   pom.xml
    	新文件：   src/main/java/edu/wust/eam/EamApplication.java
    	新文件：   src/main/resources/application.properties
    	新文件：   src/test/java/edu/wust/eam/EamApplicationTests.java

---
+++ src/main/java/edu/wust/eam/EamApplication.java
@@ -0,0 +1,13 @@
+package edu.wust.eam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/wust/eam/EamApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.wust.eam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
